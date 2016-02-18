c9a5fda8d24db18736922418dd139614afd67d71
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-02-18 16:53:00 -0600

    Se agregaron comentarios para el endpoint de users

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,21 @@
 var router = require('express').Router()
 
-/**
- * @api {post} /users Create new user
- * @apiDescription Create a new user
- * @apiName CreateUser
- * @apiGroup Admin
- * @apiVersion 0.0.1
- *
- * @apiExample {curl} CURL Example
- * curl -X POST http://localhost/api/users
- *
- * @apiParam (RequestBody) {String} username, email and password
- */
 router.route('/users')
+
+  /**
+   * @api {post} /users Create new user
+   * @apiDescription Endpoint to create a new user, without his profile
+   * @apiName CreateUser
+   * @apiGroup Admin
+   * @apiVersion 0.0.1
+   *
+   * @apiExample {curl} CURL Example:
+   * curl -X POST http://localhost/api/users
+   *
+   * @apiParam (RequestBody) {String} username The username that will be in the system
+   * @apiParam (RequestBody) {String} email The email to use in the login
+   * @apiParam (RequestBody) {String} password The password to use in the login
+   */
   .post(function (req, res) {
     res.status(201).json({
       status: 'ok',
