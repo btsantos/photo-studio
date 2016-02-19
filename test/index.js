4a2d1da5bde751cc8161284638169481b9166de1
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-02-18 23:06:41 -0600

    Se cambio la estructura del proyecto

--- test/index.js
+++ test/index.js
@@ -1,12 +1,11 @@
 var test = require('tape')
 var request = require('supertest')
-var app = require('../server')
+var app = require('../lib/app')
 
 test('POST /users', function (t) {
   request(app)
     .post('/api/users')
-    .send({username: 'mike', email: 'mike@gmail.com'})
-    .expect(201)
+    .send({username: 'mike', email: 'mike@gmail.com', password: '12345'})
     .end(function (err, res) {
       t.notOk(err, 'Should not a error')
       t.equal(res.statusCode, 201, 'Should be to get 201 code')
