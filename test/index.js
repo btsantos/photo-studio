98e0bba0cfe5170de160054cd37ce79e6bbe2c9b
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-02-20 19:05:46 -0600

    Method to save a user after to created it in the db

--- test/index.js
+++ test/index.js
@@ -2,14 +2,14 @@ var test = require('tape')
 var request = require('supertest')
 var app = require('../lib/app')
 
+var user = {username: 'mike', email: 'mike@gmail.com', password: '12345'}
+
 test('POST /users', function (t) {
   request(app)
     .post('/api/users')
-    .send({username: 'mike', email: 'mike@gmail.com', password: '12345'})
+    .send(user)
     .end(function (err, res) {
       t.equal(err, null, 'Error shoudl be null')
-      t.equal(res.body.length, 1, 'Should be just one object')
-      t.equal(res.body[0]._id.length, 24, 'Should have one Id of 24')
       t.equal(res.statusCode, 201, 'Should be to get a 201 code')
       t.end()
     })
