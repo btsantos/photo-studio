ebf1cac16dbf5bbf3b90137bb88a45d0217607a3
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-02-22 18:36:43 -0600

    se crearon test para el endpoint /users/:id

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ var request = require('supertest')
 var app = require('../lib/app')
 
 var user = {username: 'mike', email: 'mike@gmail.com', password: '12345'}
+var id_user
 
 test('POST /users', function (t) {
   request(app)
@@ -11,6 +12,19 @@ test('POST /users', function (t) {
     .end(function (err, res) {
       t.equal(err, null, 'Error shoudl be null')
       t.equal(res.statusCode, 201, 'Should be to get a 201 code')
+      id_user = res.body.data.user.id
+      t.end()
+    })
+})
+
+test('GET /users/:id', function (t) {
+  request(app)
+    .get('/api/users/' + id_user)
+    .end(function (err, res) {
+      t.equal(err, null, 'err should be null')
+      t.equal(res.body.error, false, 'Should be false the attribute error to the response')
+      t.equal(res.body.data.user.id, id_user, 'Should be the same id for this user')
+      t.equal(res.statusCode, 200, 'Should be sucess the request with status code equal to 200')
       t.end()
     })
 })
