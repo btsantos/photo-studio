5a0a2c0d9011b1521e62c8958f5d2a56f6efd238
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-02-27 00:17:23 -0600

    knex's version was changed to 0.9.0

--- test/profiles.js
+++ test/profiles.js
@@ -1,12 +1,6 @@
 var test = require('tape')
-var User = require('../lib/db/models/user')
 
 test('Create profile for an user', function (t) {
-  User.forge({id: 1})
-  .fetch()
-  .then(function (user) {
-    console.log(user)
-  })
   t.ok(true, 'testing')
   t.end()
 })
