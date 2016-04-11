9211de9195a725f140c90d8430a0e93800032157
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-04-11 18:57:23 -0500

    Model customer was created

---
+++ test/models/customers.js
@@ -0,0 +1,9 @@
+// var expect = require('chai').expect
+var customer = require('../lib/models/customers')()
+
+// describe('Model Customer', function () {
+//   it('should has a Login function', function (done) {
+//     expect(customer).to.has.property('login')
+//     done()
+//   })
+// })
