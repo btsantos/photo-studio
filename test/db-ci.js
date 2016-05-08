3de222ca2597993bb7db7ba5d9b3e38f0712505f
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-05-08 18:47:02 -0500

    Script to implement Continuous Database Integration was created and configured to add just users

--- test/db-ci.js
+++ test/db-ci.js
@@ -1,15 +1,45 @@
 var mongoose = require('mongoose')
 var config = require('../config')
+var User = require('../src/models/user')
 
 mongoose.connect(config.mongodbUri)
 
+var db = mongoose.connection
+
 // Delete database
-mongoose.connection.on('open', function () {
+db.on('open', function (err) {
   mongoose.connection.db.dropDatabase(function (err) {
     if (!err) {
       console.log('Database deleted')
     }
-    process.exit(0)
   })
 })
 
+// Create database
+mongoose.createConnection(config.mongodbUri)
+db = mongoose.connection
+
+// Create Collections with data
+// Users
+db.on('open', function (err) {
+  console.log('New Database')
+  if (!err) {
+    console.log("Inserting User's Datas...")
+    var user = new User()
+    user.username = 'mike'
+
+    user.save(function (err, doc) {
+      if (!err) {
+        console.log(doc)
+        db.close()
+      }
+    })
+  }
+})
+
+db.on('close', function (err) {
+  if (!err) {
+    console.log('Continuous Database Integration was successful')
+    process.exit(0)
+  }
+})
