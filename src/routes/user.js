1a226f787a2ea726d6b1acd4d17e5a9468d0a5d6
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-05-10 17:01:09 -0500

    API Documentation was added

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,32 @@ var router = require('express').Router()
 var User = require('../models/user')
 var config = require('../../config')
 
+/**
+ * HTTP Methods
+ *
+ * 1.- GET
+ *     Description:
+ *     The client sends a GET request to ask for a representation of a resource, identified by a URL.
+ *
+ *     Status Code:
+ *     The most common response code to a GET request is 200(ok). Redirect codes like 301 (Moved Permanently)
+ *     are also commom.
+ *
+ * 2.- DELETE
+ *     Description:
+ *     The client sends a DELETE request when it wants a resource to go away. The client wants the server
+ *     to destroy the resource and never refer to it again.
+ *
+ *     Status Code:
+ *     If a DELETE request succeeds, the possible status code are:
+ *     1.- 200 Ok
+ *         It's deleted, and here is a message about that
+ *     2.- 204 No Content
+ *         It's deleted, and I don't have anything more to say about it
+ *     3.- 202 Accepted
+ *         I will delete it later
+ */
+
 router.route('/users')
   /**
    * @api {post} /users Create new user
@@ -116,13 +142,6 @@ router.route('/users/:id')
    * curl DELETE http://localhost:3000/v1/users/123456
    */
   .delete(function (req, res) {
-    // Status Code
-    // 1.- 200 Ok 
-    //     => It's deleted, and here is a message about that
-    // 2.- 204 No Content
-    //     => It's deleted, and I don't have anything more to say about it
-    // 3.- 202 Accepted
-    //     => I will delete it later
     User.findById(req.params.id, function (err, user) {
       if (!err) {
         if (user) {
